0a62245c95543f70404cda4f64080219d0fc6fe1
Capibarik <len.leha@list.ru>
2025-05-01 01:36:15 +0800

    Сделана первая команда в консоли - clear

---
+++ PanCrackContent/js/console_utils.js
@@ -0,0 +1,16 @@
+.pragma library
+
+function clear(column) {
+    // Команда CLEAR (только эту команду мы выполняем после всего, она РЕДАКТИРУЕТ консоль)
+    // console.log("COMMAND CLEAR")
+    console.log("BEFORE COLUMN.Y", column.y)
+    for (let i = column.children.length - 1; i >= 1; i--) {
+        if (column.children[i] !== column) {
+            column.children[i].destroy()
+        }
+    }
+    // поднимаемся до конца наверх
+    column.y = 25       // начальное значение column.y
+}
+
+
---
+++ cmake/cominf.cmake
@@ -0,0 +1,13 @@
+# директория, где хранятся данные о командах
+set(COMINF_SRC_DIR ${CMAKE_SOURCE_DIR}/App/cominf)
+set(COMINF_BNR_DIR ${CMAKE_BINARY_DIR}/cominf)
+
+file(MAKE_DIRECTORY ${COMINF_BNR_DIR})
+
+add_custom_command(
+    TARGET ${CMAKE_PROJECT_NAME}
+    COMMAND ${CMAKE_COMMAND} -E copy
+        "${COMINF_SRC_DIR}/clear.json"
+        "${COMINF_BNR_DIR}/clear.json"
+    COMMENT "Копирование данных команд в json-файлах"
+)
