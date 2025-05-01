f6e25fee570dce723d7863089ae799026815b5fb
Capibarik <len.leha@list.ru>
2025-05-01 13:30:32 +0800

    Нано правки

--- qds.cmake
+++ qds.cmake
@@ -4,7 +4,6 @@
 add_subdirectory(PanCrackContent)
 add_subdirectory(PanCrack)
 add_subdirectory(App)
-add_subdirectory(Dependencies)
 
 qt6_add_resources(${CMAKE_PROJECT_NAME} MainResource
     PREFIX "/qt/qml"
