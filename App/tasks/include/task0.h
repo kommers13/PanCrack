991382d70d7dbc4e9c7d61a804293f9eb0ce72eb
kommers13 <99.3333@mail.ru>
2025-05-11 21:39:08 +0800

    task0 ready

--- App/tasks/include/task0.h
+++ App/tasks/include/task0.h
@@ -1,17 +1,20 @@
 #ifndef TASK0_H
 #define TASK0_H
 #include <graph.h>
+#include "task12.h"
 #include <iostream>
 #include <algorithm>
-class task0
-{
-public:
-    void input(Graph g, string choose);
+#include <string>
+#include <stack>
+using namespace std;
+namespace task0 {
+
+    string input(Graph g, string choose);
     string check_deg(Graph g);
     string cnt_comp_connect(Graph g);
     string if_eulers_graph(Graph g);
     string if_bipart_graph(Graph g);
 
-};
+}
 
 #endif // TASK0_H
--- App/tests/testtask0.cpp
+++ App/tests/testtask0.cpp
@@ -1,7 +1,22 @@
 #include "include/testtask0.h"
-
+using namespace std;
 void TestTask0::test(){
-    std::cout << "\nEMPTY TEST\n\n";
+    Graph g(6);
+    g.add_edge(0,3,1);
+    g.add_edge(1,4,1);
+    g.add_edge(2,5,1);
+    string ans1 = "0: 1\n1: 1\n2: 1\n3: 1\n4: 1\n5: 1\n";
+    string prog_ans1 = task0::input(g, "0");
+    string prog_ans4 = task0::input(g, "1");
+    string prog_ans2 = task0::input(g, "2");
+    string prog_ans3 = task0::input(g, "3");
+    string ans3 = "Yes, current Graph is bipart graph";
+    string ans4 = "Graph has component connection 3";
+    string ans2 = "No, current Graph isn't Eulers graph";
+    //cout <<prog_ans4 << '\n' << ans4;
+    QCOMPARE(ans4, prog_ans4);
+    QCOMPARE(ans2, prog_ans2);
+    QCOMPARE(ans3, prog_ans3);
 }
 Q_DECLARE_METATYPE(TestTask0)
 #include "testtask0.moc"
